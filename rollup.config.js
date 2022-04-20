99346af76096c377e3a10615832983c6c201d4ad
Justin Hartwig <65766832+justin-hartwig@users.noreply.github.com>
2022-04-20 11:33:55 +0200

    npm Package für Kompilierung von SCSS hinzugefügt

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,29 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript';
+import scss from 'rollup-plugin-scss';
 
-export default {
-    input: "./src/index.ts",
-    output: {
-        format: "iife",
-        file: "./build/bundle.js"
+export default [
+    {
+        input: "./src/index.ts",
+        output: {
+            format: "iife",
+            file: "./build/bundle.js"
+        },
+        plugins: [
+            typescript({
+                tsconfig: "./tsconfig.json",
+                noEmitOnError: true
+            })
+        ],
     },
-    plugins: [
-        typescript({
-            tsconfig: "./tsconfig.json",
-            noEmitOnError: true
-        })
-    ]
-};
+    {
+        input: './styling/style.js',
+        output: {
+            file: 'output.js',
+            format: 'esm'
+        },
+        plugins: [
+            scss()
+        ]
+    }
+];
