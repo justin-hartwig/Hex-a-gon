0196620f9904fcc1c6ac552dd779424fc2639ce7
Justin Hartwig <65766832+justin-hartwig@users.noreply.github.com>
2022-04-20 14:45:06 +0200

    Farbauswahl durch Hexagon ersetzt

--- src/colorCoice.ts
+++ src/colorCoice.ts
@@ -12,14 +12,11 @@ let answers :  colorChoice[] = [];
 function createColorChoice(hexValue : string) : colorChoice {
     const element : HTMLElement = document.createElement('div');
     element.classList.add("color-choice");
-    element.style.backgroundColor = hexValue;
     element.addEventListener("click", e => validateAnswer(e.target as HTMLElement));
     element.innerHTML = 
     `<svg width="121" height="105" viewBox="0 0 121 105" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M90.92 0.960022L120.92 52.922L90.92 104.884H30.92L0.920044 52.922L30.92 0.960022H90.92Z" fill="black"/>
+    <path d="M90.92 0.960022L120.92 52.922L90.92 104.884H30.92L0.920044 52.922L30.92 0.960022H90.92Z" fill="${hexValue}"/>
     </svg>`;
-    //debug
-    element.textContent = hexValue;
 
     const choice : colorChoice = { value : hexValue, domElement: element};
     return choice;
