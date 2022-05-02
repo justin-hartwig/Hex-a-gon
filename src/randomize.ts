f228efac7dc4fbb668c644b0376c0ffdf29cabef
Justin Hartwig <65766832+justin-hartwig@users.noreply.github.com>
2022-05-02 19:03:44 +0200

    Added more comments

--- src/randomize.ts
+++ src/randomize.ts
@@ -1,5 +1,11 @@
-//https://stackoverflow.com/questions/58325771/how-to-generate-random-hex-string-in-javascript IceMetalPunk
-function randomHexValue() :string{
+
+/** 
+ * Returns a random hex color value by generating random hex values in a loop and adding them to a string.
+ * https://stackoverflow.com/questions/58325771/how-to-generate-random-hex-string-in-javascript IceMetalPunk
+ * 
+ * @returns {string} Random hex color value.
+*/
+function randomHexValue() : string{
     let randomHexString = '#';
     for (let i = 0; i < 6; ++i) {
         randomHexString += (Math.floor(Math.random() * 16)).toString(16);
@@ -7,7 +13,12 @@ function randomHexValue() :string{
     return randomHexString;
 }
 
-//Schuffle Algorithm by Fisher–Yates
+/** 
+ * Shuffels a given array with the algorithm by Fisher–Yates.
+ * 
+ * @property {any[]} array The Array to be shuffled.
+ * @returns {any[]} The shuffled array.
+*/
 function shuffleArray(array : any[]) : any[] {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
--- src/validation.ts
+++ src/validation.ts
@@ -1,6 +1,16 @@
 import { colorChoice, correctAnswer, getColorChoiceBySVGElement, removeChoice } from "./colorCoice";
 import { disableColorChoice, enablePlayAgain, updateAnswerMessage, updateAnswers } from "./domManipulation";
 
+/** 
+ * Validates an answer by the clicked SVGElement.
+ * The clicked element is used to determine the color choice it belongs to.
+ * This choice is compared to the correct answer.
+ * If they don't match, the choice is removed.
+ * If they match, the play again button is enabled and all choices are disabled.
+ * The Answer message is updated depending on the result.
+ * 
+ * @property {SVGElement} target The clicked SVGElement which color choice should be validated.
+*/
 function validateAnswer(target : SVGElement) : void {
     const choice : colorChoice = getColorChoiceBySVGElement(target);
     updateAnswerMessage(choice.value === correctAnswer);
